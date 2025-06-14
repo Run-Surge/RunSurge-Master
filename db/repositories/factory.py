39d28c401d199593279fc8ec9950bf068d0f07e0
MostafaMagdyy <mustafa.rahman02@eng-st.cu.edu.eg>
2025-06-14 23:14:23 +0300

    Adding Task repositery

--- db/repositories/factory.py
+++ db/repositories/factory.py
@@ -4,7 +4,7 @@ from db.session import get_session
 from .job import JobRepository
 from .node import NodeRepository
 from .user import UserRepository
-
+from .task import TaskRepository
 
 class RepositoryFactory:
     def __init__(self, session: Session):
@@ -12,6 +12,7 @@ class RepositoryFactory:
         self._job_repo = None
         self._node_repo = None
         self._user_repo = None
+        self._task_repo = None
 
     @property
     def user(self) -> UserRepository:
@@ -30,6 +31,14 @@ class RepositoryFactory:
         if self._node_repo is None:
             self._node_repo = NodeRepository(self.session)
         return self._node_repo
+    
+    @property
+    def task(self) -> TaskRepository:
+        if self._task_repo is None:
+            self._task_repo = TaskRepository(self.session)
+        return self._task_repo
+    
+
     
 
 def get_repositories(session: Session = Depends(get_session)) -> RepositoryFactory:
